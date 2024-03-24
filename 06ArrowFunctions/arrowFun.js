efb60cead2f2906de68c4f980da865595c29116f
Seyed Muzaffar <iammseyed@proton.me>
2024-03-24 22:01:36 +0530

    updated arrowFun

--- 06ArrowFunctions/arrowFun.js
+++ 06ArrowFunctions/arrowFun.js
@@ -1,10 +1,10 @@
 const nonArwFun = function(name){
     console.log(`my name is ${name}`)
 } 
-nonArwFun('Muzafar')
+nonArwFun('name')
 
 const ArwFun = (name) => `my name is ${name}`
-console.log(ArwFun('muza'))
+console.log(ArwFun('name'))
 /*In the arrow function we skip the word 'function', return type  
 and curly braces. In place of that we start straight with the function 
 argument and then the arrow sign followed by the code which function is 
